a0da4c427c1bc09b917faa1e2fbed28b981cd7e2
unknown <ecenjyi@E7D89D67962706.ericsson.se>
2014-09-30 10:15:35 +0800

    hash with error

--- count_num.c
+++ count_num.c
@@ -1,22 +1,91 @@
-#include<stdio.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+#define HASHNUMBER 60
+typedef struct structCharHash{
+	char *word;
+	int number;
+	struct structCharHash * next;
+}charHash;
+static charHash charHashTable[HASHNUMBER];
+void insertWord(char * pword){
+	printf("pword:%s\n",pword);
+	char * lword=pword;
+	int hashNum=calcWordHash(lword);
+	charHash p=charHashTable[hashNum];
+
+	if(p.word==NULL){
+		charHash pc;
+		pc.word=pword;
+		//printf("pc.word:%s\n",pc.word);
+		pc.number=1;
+		pc.next=NULL;
+		charHashTable[hashNum]=pc;
+		//printf("this word  is %s ,and the number is %d\n",pc.word,pc.number);
+		return ;
+	}
+	charHash *ppre=&charHashTable[hashNum];
+	charHash *pcur=ppre;
+	while(pcur!=NULL){
+		if(strcmp(pcur->word,pword)==0){
+			pcur->number+=1;
+			//printf("this word  is %s ,and the number is %d\n",pcur->word,pcur->number);
+			return;
+		}
+		//printf("this word  is %s ,and the number is %d\n",pcur->word,pcur->number);
+		pcur=pcur->next;
+		ppre=pcur;
+	}
+	charHash * pnewWord=(charHash *)malloc(sizeof(charHash));
+	ppre->next=pnewWord;
+}
+
+int calcWordHash(char *pword){
+	int ret = 0;
+	while(*pword!='\0'){
+		ret+=*pword;
+		pword++;
+	}
+	printf("ret:%d\n",ret%HASHNUMBER);
+	return ret%HASHNUMBER;
+}
+
+void initHashTable(){
+	memset(charHashTable,0,sizeof(charHashTable));
+}
 int main(){
+	initHashTable();
 	printf("input the route and the name of file\n");
 	char * file;
 	file=(char*)malloc(10*sizeof(char));
 	gets(file);
 	printf("file name:%s\n",file);
-	int num=count(file);
-	printf("\n%d",num);
+	int num=count_num(file);
+	printf("\n%d\n",num);
+	int i=0;
+	for (;i<HASHNUMBER;i++){
+		charHash *pch = &charHashTable[i];			
+		if (pch->word !=NULL){
+			while(pch != NULL){
+				printf("this word  is %s ,and the number is %d\n",pch->word,pch->number);
+				pch= pch->next;
+			}
+		}
+
+	    //printf("word is %s and number is %d \n",charHashTable[i].word,charHashTable[i].number);
+	}
+	free(file);
+	file=NULL;
 	return 0;
 }
-int count(char * file){
+int count_num(char * filename){
 	FILE *fp;
 	char c;
 	int num=0;//the num of ' '
 	//printf("%s\n",file);
 	//sprintf(file,file);
-	fp=fopen(file,"r");
+	fp=fopen(filename,"r");
 	if(fp==NULL){
 		printf("fail to open file");
 		return -1;
@@ -34,14 +103,26 @@ int count(char * file){
 		//printf("%c",c);
 
 	}*/
+	char word[15];
 	
 	while(!feof(fp)){
 		c=fgetc(fp);
 		if(c>='a'&&c<='z'||c>='A'&&c<='Z'){
+			int i=0;
+			for(;i<15;i++){
+				word[i]='\0';
+			}
+			i=0;
+			word[i]=c;
+			i++;
 			c=fgetc(fp);
 			while(c>='a'&&c<='z'||c>='A'&&c<='Z'){
+				word[i]=c;
+				i++;
 				c=fgetc(fp);
 			}
+			printf("%s\n",word);
+			insertWord(word);
 			num++;
 		}
 	}
@@ -51,4 +132,5 @@ int count(char * file){
 	//printf("%s\n",mytext);
 	//fclose(fp);
 	return num;
+
 }
